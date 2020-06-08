8de89f3bfabdcb0376561d8862c7b0eeaa66a91e
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-06-08 21:05:13 +0200

    kod.22.3.example: rozbudwa klienta

---
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloCardDto {
+
+    private String name;
+    private String description;
+    private String pos;
+    private String idList;
+}
