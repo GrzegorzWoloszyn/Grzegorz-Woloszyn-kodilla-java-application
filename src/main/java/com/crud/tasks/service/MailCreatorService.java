b2db3c926b4fe54bd108f0d33bd145d86a11ca1a
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-08-28 16:03:30 +0200

    kod.32.2.task

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,20 @@ public class MailCreatorService {
     @Autowired
     private AdminConfig adminConfig;
 
+    @Autowired
+    private CompanyDetails companyDetails;
+
+    private String goodbye = "Sincerely, ";
+
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("task_url", "http://localhost:8888/tasks_frontend/");
-        context.setVariable("button", "visit website");
-        context.setVariable("admin_mail", adminConfig.getAdminName());
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyDetails.getAppName());
+        context.setVariable("company_details", companyDetails.getAppName() + "\n" +
+                companyDetails.getOwnerName() + " " + companyDetails.getOwnerSurname());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
